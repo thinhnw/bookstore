5ab6ae3356b9747e0827a373436b3552647bb7cd
thinhnw <thinhnv0412@gmail.com>
2021-02-08 09:54:29 +0700

    Overload listCategory()

--- src/main/java/com/bookstore/service/CategoryService.java
+++ src/main/java/com/bookstore/service/CategoryService.java
@@ -7,8 +7,11 @@ import com.bookstore.entity.CategoryEntity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 public class CategoryService {
@@ -27,14 +30,32 @@ public class CategoryService {
     }
 
 
-    public void listCategory() {
-        List<CategoryEntity> listCategory = categoryDAO.listAll();
-        request.setAttribute("listCategory", listCategory);
-
+    public void listCategory() throws ServletException, IOException {
+        listCategory(null);
     }
 
-    public void createCategory() {
-        CategoryEntity ctg = new CategoryEntity();
-        categoryDAO.create(ctg);
+    public void listCategory(String message) throws ServletException, IOException {
+        List<CategoryEntity> listCategory = categoryDAO.listAll();
+        request.setAttribute("listCategory", listCategory);
+        if (message != null) {
+            request.setAttribute("message", message);
+        }
+        RequestDispatcher rd = request.getRequestDispatcher("category_list.jsp");
+        rd.forward(request, response);
+    }
+
+    public void createCategory() throws ServletException, IOException {
+        String name = request.getParameter("name");
+        CategoryEntity ctg = categoryDAO.findByName(name);
+        if (ctg != null) {
+            String message = "Could not create category. This category already exists!";
+            request.setAttribute("message", message);
+            RequestDispatcher rd = request.getRequestDispatcher("message.jsp");
+            rd.forward(request, response);
+        } else {
+            categoryDAO.create(new CategoryEntity(name));
+            String message = "Category created successfully";
+            listCategory(message);
+        }
     }
 }
